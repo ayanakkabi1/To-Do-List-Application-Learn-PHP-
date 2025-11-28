bc62550fcb84372eccfbd0f23236458a26a87871
ayanakkabi1 <aynour200609@gmail.com>
2025-11-28 12:41:37 +0100

    feat(html): add task creation form

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ma Todo App</title>
+  </head>
+  <body>
+    <h1>Ajouter une tache</h1>
+    <form action="add_task.php" method="POST">
+    <input type="text" name="task">
+    <button type="submit">Add</button>
+    </form>
+        
+  </body>
+</html>
